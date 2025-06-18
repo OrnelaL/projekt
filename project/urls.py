e3a5374c069c393f538d50f03d1bcc729d22d346
OrnelaL <ornelalikaaa2024@gmail.com>
2025-06-18 12:56:42 +0200

    add models

--- project/urls.py
+++ project/urls.py
@@ -1,3 +1,4 @@
+
 """
 URL configuration for project project.
 
@@ -16,8 +17,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# Shtuar nga django media
+from django.conf import settings
+from django.conf.urls.static import static
+# Shtuar nga django media fund 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("app.urls"))
 ]
+# Shtuar nga django media
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
